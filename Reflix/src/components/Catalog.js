50b9a40d4d9b6e312951ac61bece19f3d4a6ba67
Itay.Talias <itay.talias@gmail.com>
2022-11-09 17:45:26 +0200

    adding title to catalog

--- Reflix/src/components/Catalog.js
+++ Reflix/src/components/Catalog.js
@@ -6,6 +6,9 @@ class Catalog extends Component {
     render() {
         return (
             <div id="catalog">
+                <h1 id="title-catalog">
+                    WELCOME BACK {this.props.match.match.params.name}!!
+                </h1>
                 {this.props.catalog.map((m, i) => (
                     <MovieCard key={i} movie={m}></MovieCard>
                 ))}
