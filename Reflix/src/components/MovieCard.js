b4e246d249d9aedb776fd442e922e8a11935fa17
Itay.Talias <itay.talias@gmail.com>
2022-11-10 00:23:45 +0200

    adding pop up to card

--- Reflix/src/components/MovieCard.js
+++ Reflix/src/components/MovieCard.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "../style/MovieCard.css";
 import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 class MovieCard extends Component {
     render() {
@@ -22,6 +24,9 @@ class MovieCard extends Component {
                     <h4>{this.props.movie.title}</h4>
                     <p>{this.props.movie.year}</p>
                 </div>
+                <Popup trigger={<button> Info</button>}>
+                    <div>{this.props.movie.descrShort}</div>
+                </Popup>
             </div>
         );
     }
