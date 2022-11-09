9f5819dc9b121dbd45f20322ef757cb807c534e1
Itay.Talias <itay.talias@gmail.com>
2022-11-10 00:23:07 +0200

    adding swiper

--- Reflix/src/components/Catalog.js
+++ Reflix/src/components/Catalog.js
@@ -1,17 +1,38 @@
 import React, { Component } from "react";
 import "../style/Catalog.css";
-import MovieCard from "./MovieCard";
+import MovieSwiper from "./MoviesSwiper";
 
 class Catalog extends Component {
+    getRentMovies = () => {
+        return this.props.catalog.filter((m) => m.isRented === true);
+    };
+    getUnrentMovies = () => {
+        return this.props.catalog.filter((m) => m.isRented !== true);
+    };
     render() {
         return (
             <div id="catalog">
                 <h1 id="title-catalog">
                     WELCOME BACK {this.props.match.match.params.name}!!
                 </h1>
-                {this.props.catalog.map((m, i) => (
-                    <MovieCard key={i} movie={m}></MovieCard>
-                ))}
+                {this.getRentMovies().length !== 0 ? (
+                    <div>
+                        <hr></hr>
+                        <h1 className="title">Rented:</h1>
+                        <MovieSwiper
+                            movies={this.getRentMovies()}
+                        ></MovieSwiper>
+                    </div>
+                ) : null}
+                {this.getUnrentMovies().length !== 0 ? (
+                    <div>
+                        <hr></hr>
+                        <h1 className="title">Catalog:</h1>
+                        <MovieSwiper
+                            movies={this.getUnrentMovies()}
+                        ></MovieSwiper>
+                    </div>
+                ) : null}
             </div>
         );
     }
