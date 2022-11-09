696923e6d382f8f5d30cda943b414280ce815323
Itay.Talias <itay.talias@gmail.com>
2022-11-10 00:22:18 +0200

    mock data

--- Reflix/src/App.js
+++ Reflix/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            users: ["Itay", "Lotan", "Liam", "Tal"],
+            users: ["ITAY", "LOTAN", "LIAM", "TAL"],
             catalog: [
                 {
                     id: 0,
@@ -55,6 +55,60 @@ class App extends Component {
                     img: "https://images-na.ssl-images-amazon.com/images/I/51ArFYSFGJL.jpg",
                     descrShort:
                         "Basically the same as the original, except now Hermi-- Emma Wattson plays Belle, fittingly so some would say, given how actively progressive she is regarding women's rights. Rumor has it that in the bonus scenes she whips out a wand and turns Gaston into a toad, but in order to watch those scenes you need to recite a certain incantation.",
+                },
+                {
+                    id: 5,
+                    isRented: false,
+                    title: "Black Adam",
+                    year: 2022,
+                    img: "https://amc-theatres-res.cloudinary.com/image/upload/f_auto,fl_lossy,h_465,q_auto,w_310/v1663085107/amc-cdn/production/2/movies/62300/62331/PosterDynamic/142915.jpg",
+                    descrShort:
+                        "Nearly 5,000 years after he was bestowed with the almighty powers of the Egyptian gods--and imprisoned just as quickly--Black Adam is freed from his earthly tomb, ready to unleash his unique form of justice on the modern world.",
+                },
+                {
+                    id: 6,
+                    isRented: false,
+                    title: "Shang-chi",
+                    img: "https://lumiere-a.akamaihd.net/v1/images/p_shangchiandthelegendofthetenringshomeentupdate_22055_7b62fa67.jpeg?region=0%2C0%2C540%2C800",
+                    year: 2021,
+                    descrShort:
+                        "Shang-Chi, the master of weaponry-based Kung Fu, is forced to confront his past after being drawn into the Ten Rings organization.",
+                },
+                {
+                    id: 7,
+                    isRented: false,
+                    title: "Top Gun",
+                    year: 2022,
+                    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKUtghkEMN-49NPCzIAfGhSS7_i2d2ymjpDw&usqp=CAU",
+                    descrShort:
+                        "After thirty years, Maverick is still pushing the envelope as a top naval aviator, but must confront ghosts of his past when he leads TOP GUN's elite graduates on a mission that demands the ultimate sacrifice from those chosen to fly it.",
+                },
+                {
+                    id: 8,
+                    isRented: false,
+                    title: "Nope",
+                    year: 2022,
+                    img: "https://i.ytimg.com/vi/oJ_VO7tWIE4/movieposter_en.jpg",
+                    descrShort:
+                        "The residents of a lonely gulch in inland California bear witness to an uncanny and chilling discovery.",
+                },
+                {
+                    id: 9,
+                    isRented: true,
+                    title: "Thor",
+                    year: 2022,
+                    img: "https://cdn.moviestillsdb.com/storage/posters/43/10648342_150.jpg",
+                    descrShort:
+                        "Thor enlists the help of Valkyrie, Korg and ex-girlfriend Jane Foster to fight Gorr the God Butcher, who intends to make the gods extinct.",
+                },
+                {
+                    id: 10,
+                    isRented: true,
+                    title: "Gun Crazy",
+                    year: 1950,
+                    img: "https://cdn.moviestvnetwork.com/iKtQ2-1550175292-1919-movie-GunCrazy_MOVIES_800x1187.jpg",
+                    descrShort:
+                        "Two disturbed young people release their fascination with guns through a crime spree.",
                 },
             ],
         };
